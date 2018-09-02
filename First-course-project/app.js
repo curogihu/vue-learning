4c67f4d4894d96a7001deef34e9fa0110beac912
curogihu <curoasg@gmail.com>
2018-09-02 21:43:22 +0900

    lecture 51
    implementing a  “attack“ method

--- First-course-project/app.js
+++ First-course-project/app.js
@@ -11,6 +11,38 @@ new Vue({
 			this.gameIsRunning = true;
 			this.playerHealth = 100;
 			this.monsterHealth = 100;
+		},
+
+		attack: function() {
+			this.monsterHealth -= this.getRandomDamage(10, 3);
+
+			if(this.monsterHealth <= 0) {
+				alert('You won!');
+				this.gameIsRunning = false;
+			}
+
+			this.playerHealth -= this.getRandomDamage(12, 5);
+
+			if(this.playerHealth <= 0) {
+				alert('You lost!');
+				this.gameIsRunning = false;
+			}
+		},
+
+		getRandomDamage: function(max, min) {
+			return Math.max(Math.floor(Math.random() * max) + 1, min);
+		},
+
+		specialAttack: function() {
+
+		},
+
+		heal: function() {
+
+		},
+
+		giveUp: function() {
+
 		}
 	}
 });
